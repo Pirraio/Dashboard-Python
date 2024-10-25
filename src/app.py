d3ff02e847e0f4435d86a369c8069998e31eca0b
Pirraio <g.teixeira7x@gmail.com>
2024-10-25 00:08:06 -0300

    add new graph and customize template

--- src/app.py
+++ src/app.py
@@ -11,17 +11,18 @@ app.config.suppress_callback_exceptions = True # configuração de certificaçã
 
 
 # utilizado para dicrecionar os caminhos para os arquivos Excel que contêm os dados que serão analisados
-file_path_turma = os.path.join(os.path.dirname(__file__), "tabela_desempenho_da_turma.xlsx")
-file_path_estudante = os.path.join(os.path.dirname(__file__), "desempenho_estudante_turma.xlsx")
-file_path_submissoes = os.path.join(os.path.dirname(__file__), "submissoes_por_dia.xlsx")
-file_path_dificuldade = os.path.join(os.path.dirname(__file__), "tabela_de_desempnho_por_dificuldade.xlsx")
-
+file_path_turma = os.path.join(os.path.dirname(__file__), "data/tabela_desempenho_da_turma.xlsx")
+file_path_estudante = os.path.join(os.path.dirname(__file__), "data/desempenho_estudante_turma.xlsx")
+file_path_submissoes = os.path.join(os.path.dirname(__file__), "data/submissoes_por_dia.xlsx")
+file_path_dificuldade = os.path.join(os.path.dirname(__file__), "data/tabela_de_desempnho_por_dificuldade.xlsx")
+file_path_question = os.path.join(os.path.dirname(__file__), "data/questions_by_difficulty_with_title.xlsx")
 
 # carrega os dados dos arquivos Excel
 df_turma = pd.read_excel(file_path_turma)
 df_estudante = pd.read_excel(file_path_estudante)
 df_submissoes = pd.read_excel(file_path_submissoes)
 df_dificuldade = pd.read_excel(file_path_dificuldade)
+df_questions_difficulty = pd.read_excel(file_path_question)
 
 # modifica o DataFrame reformulando os dados (desempenho estudante)
 df_estudante_exp = df_estudante.explode(['expressoes', 'estrutura_de_decisao', 'repeticao_condicional', 'repeticao_contada', 'vetores'])
@@ -34,16 +35,16 @@ df_turma_long['status'] = df_turma_long['variable'].map({'hits': 'Concluído', '
 
 # usado para criar o gráfico de barras para mostrar o desempenho da turma por lista de exercícios
 fig_turma = px.bar(df_turma_long, x="list", y="value",
-                   color="status", barmode="group",
+                   color="status",
                    title="Desempenho da Turma por Lista",
                    labels={"list": "Lista de Exercícios", "value": "Número de Submissões", "status": "Status"},
-                   template="plotly_white")
+                   template="plotly_dark")
 
 # usado para criar o gráfico de linha para mostrar o número de submissões por dia
 fig_submissoes = px.line(df_submissoes, x="data", y="qnt_de_submissoes",
                          title="Número de Submissões por Dia",
                          labels={"data": "Data", "qnt_de_submissoes": "Número de Submissões"},
-                         template="plotly_white")
+                         template="plotly_dark")
 
 # modifica o DataFrame reformulando os dados (desempenho por dificuldade) e renomeando colunas
 df_dificuldade_exp = df_dificuldade.melt(id_vars='user_id', value_vars=['muito_faceis', 'faceis', 'medias', 'dificeis', 'muito_dificeis'])
@@ -53,12 +54,20 @@ df_dificuldade_exp[['Fácil', 'Médio', 'Difícil']] = pd.DataFrame(df_dificulda
 df_dificuldade_long = df_dificuldade_exp.melt(id_vars=['user_id', 'dificuldade'], value_vars=['Fácil', 'Médio', 'Difícil'])
 df_dificuldade_long['dificuldade'] = df_dificuldade_long['dificuldade'].map({'muito_faceis': 'Muito Fáceis', 'faceis': 'Fáceis', 'medias': 'Médias', 'dificeis': 'Difíceis', 'muito_dificeis': 'Muito Difíceis'})
 
+
 # usado para criar o gráfico de barras para mostrar o desempenho dos estudantes por nível de dificuldade
-fig_dificuldade = px.bar(df_dificuldade_long, x="dificuldade", y="value",
-                        color="variable", barmode="group",
+fig_dificuldade = px.histogram(df_dificuldade_long, x="dificuldade", y="value",
+                        color="variable",
                         title="Desempenho dos Estudantes por Dificuldade",
                         labels={"dificuldade": "Nível de Dificuldade", "value": "Quantidade de Submissões", "variable": "Tipo"},
-                        template="plotly_white")
+                        template="plotly_dark")
+
+
+
+fig_questions_by_difficulty = px.bar(df_questions_difficulty, x='title', y='percentage',
+                                     title='Percentual de acerto por questão',
+                                     labels={"data": "Data", "percentage": "Percentual"},
+                                     template="plotly_dark")
 
 # utilizado para criar o layout de aplicação e criar diferentes abas para cada gráfico
 app.layout = html.Div(children=[
@@ -109,6 +118,24 @@ app.layout = html.Div(children=[
                 figure=fig_dificuldade
             )
         ], className='five columns'),
+        
+        html.Div([
+            dcc.Dropdown(
+                id="dropdown_question",
+                options=[
+                    {'label': '0 - 25% (Taxa muito baixa de acertos)', 'value': 'first_quarter'},
+                    {'label': '25 - 50% (Taxa baixa de acertos)', 'value': 'second_quarter'},
+                    {'label': '50 - 75% (Taxa boa de acertos)', 'value': 'third_quarter'},
+                    {'label': '75 - 100% (Taxa muito boa de acertos)', 'value': 'fourth_quarter'},
+                ],
+                value='first_quarter'
+            ),
+            dcc.Graph(
+                id='question_difficulty',
+                 figure=fig_questions_by_difficulty
+            ),
+            
+        ], className='five columns'),
     
     ], className='custom-container'),
 ])
@@ -119,6 +146,8 @@ app.layout = html.Div(children=[
     [Input('dropdown_estudante', 'value'),
      Input('dropdown_assunto', 'value')]
 )
+
+
 # modelos de filtragem
 def update_estudante_graph(selected_user_id, selected_assunto):
     # filtragem de dados
@@ -137,11 +166,23 @@ def update_estudante_graph(selected_user_id, selected_assunto):
     fig = px.bar(df_pivot, x='user_id', y=['Concluído', 'Incompleto', 'Pendente'], 
                  title=f"Desempenho do Estudante {selected_user_id} em {selected_assunto}",
                  labels={"user_id": "ID do Estudante", "value": "Quantidade"},
-                 text_auto=True, 
-                 barmode='group',
-                 template="plotly_white")
+                 text_auto=True,
+                 template="plotly_dark")
 
     return fig
+
+# @app.callback(
+#     Output('dropdown_question', 'figure'),
+#     Input('dropdown_question', 'value')
+# )
+
+def update_question_graph(selected_percentage):
+    if selected_percentage == '0 - 25% (Taxa muito baixa de acertos)':
+        print('teste')
+        ...
+    #return df_filtered
+
+
 
 # execução do aplicativo
 
